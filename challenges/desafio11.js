d87e08b524f772f1399a1ec56d533aa9fbdc37b2
Tarick <gabre.tarick@gmail.com>
2022-12-19 00:52:23 -0300

    retorno o nome do produto, a qtde d likess e qts itens foram vendidos dos produtos q n sao =  Big Mac e McChicken

---
+++ challenges/desafio11.js
@@ -0,0 +1,13 @@
+db.produtos.find(
+    {
+        nome: {
+            $nin: ["Big Mac", "McChicken"],
+        },
+    },
+    {
+        _id: false,
+        nome: true,
+        curtidas: true,
+        vendidos: true,
+    },
+).pretty();
