751b9eface39386fd0b34f72133cee9fcdb19bb3
Tarick <gabre.tarick@gmail.com>
2022-12-19 00:31:13 -0300

    deleto os lanches com likes > 50 e retorno o nome dos lanches q restaram no banco

---
+++ challenges/desafio8.js
@@ -0,0 +1,14 @@
+db.produtos.deleteMany(
+    {
+        curtidas: {
+            $lt: 50,
+        },
+    },
+);
+
+db.produtos.find(
+    {},
+    {
+        nome: true,
+        _id: false
+    }).pretty();
